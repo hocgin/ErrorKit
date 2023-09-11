f3c3dbc055851f0e671b0a07e9d33fcaaae9875e
Cihat Gündüz <commits@cihatguenduez.de>
2023-09-11 20:01:00 +0200

    Add sample throwing code on the call site

--- Package.swift
+++ Package.swift
@@ -6,31 +6,22 @@ import CompilerPluginSupport
 
 let package = Package(
     name: "ErrorKit",
-    platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6), .macCatalyst(.v13)],
+    platforms: [.macOS(.v13), .iOS(.v16), .tvOS(.v16), .watchOS(.v9), .macCatalyst(.v16)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "ErrorKit",
-            targets: ["ErrorKit"]
-        ),
-        .executable(
-            name: "ErrorKitClient",
-            targets: ["ErrorKitClient"]
-        ),
+        .library(name: "ErrorKit", targets: ["ErrorKit"]),
+        .executable(name: "ErrorKitClient", targets: ["ErrorKitClient"]),
     ],
     dependencies: [
         // Depend on the latest Swift 5.9 prerelease of SwiftSyntax
         .package(url: "https://github.com/apple/swift-syntax.git", from: "509.0.0-swift-5.9-DEVELOPMENT-SNAPSHOT-2023-04-25-b"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        // Macro implementation that performs the source transformation of a macro.
         .macro(
             name: "ErrorKitMacros",
             dependencies: [
                 .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
-                .product(name: "SwiftCompilerPlugin", package: "swift-syntax")
+                .product(name: "SwiftCompilerPlugin", package: "swift-syntax"),
             ]
         ),
 
--- Sources/ErrorKitClient/main.swift
+++ Sources/ErrorKitClient/main.swift
@@ -1,8 +1,61 @@
 import ErrorKit
+import Foundation
 
-let a = 17
-let b = 25
+final class Database {
+   static func loadMovies(byGenre genre: Movie.Genre) throws -> [Movie] {
+      let randomInt = (0..<100).randomElement()!
+      if randomInt < 33 {
+         throw NSError.generic(code: 54934, message: "Loading movies from database failed!")
+      } else if randomInt < 66 {
+         return []
+      } else {
+         return [
+            Movie(title: "Harry Potter and the Philosopher's Stone", releaseYear: 2001),
+            Movie(title: "Harry Potter and the Chamber of Secrets", releaseYear: 2002),
+            Movie(title: "Harry Potter and the Prisoner of Azkaban", releaseYear: 2004),
+            Movie(title: "Harry Potter and the Goblet of Fire", releaseYear: 2005),
+            Movie(title: "Harry Potter and the Order of the Phoenix", releaseYear: 2007),
+            Movie(title: "Harry Potter and the Half-Blood Prince", releaseYear: 2009),
+            Movie(title: "Harry Potter and the Deathly Hallows – Part 1", releaseYear: 2010),
+            Movie(title: "Harry Potter and the Deathly Hallows – Part 2", releaseYear: 2011),
+         ]
+      }
+   }
+}
 
-let (result, code) = #stringify(a + b)
+struct Movie: Equatable {
+   let title: String
+   let releaseYear: Int
 
-print("The value \(result) was produced by the code \"\(code)\"")
+   enum Genre {
+      case action, anime, bollywood, comedy, drama
+   }
+
+   static func randomMovies(genre: Genre, count: Int) throws -> [Movie] {
+      var movies = try Database.loadMovies(byGenre: genre)
+
+      guard !movies.isEmpty else {
+         throw NSError.generic(code: 21693, message: "No movies found matching the genre '\(genre)'.")
+      }
+
+      var randomMovies: [Movie] = []
+      for _ in 0..<count {
+         guard let randomMovie = movies.randomElement() else {
+            throw NSError.generic(code: 89316, message: "Not enough movies matching the genre '\(genre)'.")
+         }
+         
+         movies.removeAll { $0 == randomMovie }
+         randomMovies.append(randomMovie)
+      }
+
+      return randomMovies
+   }
+}
+
+extension NSError {
+    static func generic(code: Int, message: String) -> NSError {
+        NSError(domain: Bundle.main.bundleIdentifier ?? "App", code: code, userInfo: [NSLocalizedDescriptionKey: message])
+    }
+}
+
+print(try Movie.randomMovies(genre: .action, count: 5).map(\.title))
