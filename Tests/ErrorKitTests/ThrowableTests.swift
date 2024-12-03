14f67e35fa7e6591fe8106921ce327963a1b5286
Cihat Gündüz <commits@cihatguenduez.de>
2024-12-03 17:55:27 +0100

    Add tests for Throwable protocol

---
+++ Tests/ErrorKitTests/ThrowableTests.swift
@@ -0,0 +1,37 @@
+import Testing
+@testable import ErrorKit
+
+enum ExplicitDescriptionError: Throwable {
+   case somethingFailed
+   case reuestTimeout
+
+   var localizedDescription: String {
+      switch self {
+      case .somethingFailed: "Something failed unexpectedly"
+      case .reuestTimeout: "Request timed out"
+      }
+   }
+}
+
+@Test
+func explicitDescriptionOutput() async throws {
+   do {
+      throw ExplicitDescriptionError.somethingFailed
+   } catch {
+      #expect(error.localizedDescription == "Something failed unexpectedly")
+   }
+}
+
+enum RawValueDescriptionError: String, Throwable {
+   case somethingFailed = "Something failed unexpectedly"
+   case reqestTimeout = "Request timed out"
+}
+
+@Test
+func rawValueDescriptionOutput() async throws {
+   do {
+      throw RawValueDescriptionError.reqestTimeout
+   } catch {
+      #expect(error.localizedDescription == "Request timed out")
+   }
+}
